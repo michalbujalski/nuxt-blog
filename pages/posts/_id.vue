af5b8a3efa22fb0b0e3e7e8359a689c7f7b798d6
Michal Bujalski <bujalski.michal@gmail.com>
2019-04-09 22:44:44 +0200

    Added post details page

---
+++ pages/posts/_id.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>{{post.title}}</h1>
+        <p>{{post.description}}</p>
+    </div>
+</template>
+<script>
+export default {
+    async asyncData ({params}) {
+        const response = await fetch(`http://5cacc01b01a0b80014dcd04f.mockapi.io/posts/${params.id}`)
+        const post = await response.json()
+        return { post }
+    }
+}
+</script>
